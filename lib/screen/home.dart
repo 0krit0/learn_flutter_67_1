acabecacce77a01ccad0a2e4e514a41b588c6fba
0krit0 <0991453668za@gmail.com>
2025-08-16 11:07:20 +0700

    Step 6: Image Widet

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,56 +5,23 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //return Row(
     return Column(
-      //mainAxisAlignment: MainAxisAlignment.start,
-      //mainAxisAlignment: MainAxisAlignment.center,
-      //mainAxisAlignment: MainAxisAlignment.end,
-      //mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          color: const Color.fromARGB(255, 255, 69, 2),
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Color.fromARGB(255, 255, 254, 240),
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
+        Image.asset(
+          "assets/images/101.jpg",
+          width: 300,
+          height: 300,
         ),
-         Container(
-          color: const Color.fromARGB(255, 255, 69, 2),
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Color.fromARGB(255, 255, 254, 240),
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-         Container(
-          color: const Color.fromARGB(255, 255, 69, 2),
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: const Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Color.fromARGB(255, 255, 254, 240),
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
+        const SizedBox(height: 20),
+        Image.network(
+          "https://ultravet-pet.com/wp-content/uploads/2023/07/cat-breed-01.webp", // ใช้ลิงก์รูปตรง (Pinterest ต้องเป็น direct image url)
+          width: 300,
+          height: 300,
         ),
       ],
-    ); // <<--- ตรงนี้ต้องมี ;
+    );
   }
 }
+
