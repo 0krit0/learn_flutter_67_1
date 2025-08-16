6475b02765c91f065999739f1c6768974e860e4e
0krit0 <0991453668za@gmail.com>
2025-08-16 10:30:15 +0700

    Step5: Container, row, Column, main axis, cross axis

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,10 +5,43 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
+    //return Row(
+    return Column(
+      //mainAxisAlignment: MainAxisAlignment.start,
+      //mainAxisAlignment: MainAxisAlignment.center,
+      //mainAxisAlignment: MainAxisAlignment.end,
+      //mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Container(
+          color: const Color.fromARGB(255, 255, 69, 2),
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: const Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              color: Color.fromARGB(255, 255, 254, 240),
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3,
+            ),
+          ),
+        ),
+         Container(
+          color: const Color.fromARGB(255, 255, 69, 2),
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: const Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              color: Color.fromARGB(255, 255, 254, 240),
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3,
+            ),
+          ),
+        ),
+         Container(
           color: const Color.fromARGB(255, 255, 69, 2),
           padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
           child: const Text(
